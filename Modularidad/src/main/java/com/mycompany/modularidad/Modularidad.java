80f145d703fc3265de7570cd6f5ab63480dec9c8
PatriciaSotalin <132688857+PatriciaSotalin@users.noreply.github.com>
2023-08-15 19:43:45 -0500

    Sotalin Adeir Modularidad

---
+++ Modularidad/src/main/java/com/mycompany/modularidad/Modularidad.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modularidad;
+
+/**
+ *
+ * @author USER
+ */
+public class Modularidad {
+//variable globales
+    String nombre;
+        final String GRUPO= "Sis12";
+    public static void main(String[] args) {
+        //variable y constates
+        String apellido="cesar";
+        final double  IVA=0.13;
+        System.out.println{"Datos locales: =+"
+                "\n Apellido"+apellido+
+                "\n IVA:"+IVA };
+        
+     
+    }
+}
